ccd77847b03475365cb0dfbccc9544c1e8a13451
Fiona Lapham <fionadlapham@gmail.com>
2018-04-04 14:38:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,9 @@
 class Triangle
 
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :a, :b, :c
 
-  def initialization (length1, length2, length3)
-  end
-
-  def equilateral
-    if length1 == length2 && length2 == length3 && length1 == length3
-    true
-  end
+  def initialization (a, b, c)
+  
   end
 
 
